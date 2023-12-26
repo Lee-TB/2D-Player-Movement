ed98d9003fee00e73b696800a01aa54aa387c294
Tommy Tran <tblyduc2412@gmail.com>
2023-12-26 21:50:41 +0700

    Coyote Time & Jump Buffering

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float horizontal;
-    private bool isFacingRight = true;
-    private Color defaultColor;
-
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float jumpingPower = 20f;
     [SerializeField] private float speed = 10f;
+
+    private float horizontal;
+    private bool isFacingRight = true;
+    private Color defaultColor;
+    private float coyoteJumpTimer;
+    private float coyoteJumpTimerMax = 0.2f;
+    private float jumpBufferingTimer;
+    private float jumpBufferingTimerMax = 0.2f;
 
     private void Start()
     {
@@ -28,10 +32,45 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         SetPlayerColor(defaultColor);
+        if (IsGrounded())
+        {
+            SetPlayerColor(Color.white);
+        }
 
+        HandleHorizontalMovement();
+
+        HandleJumping();
+
+        Flip();
+    }
+
+    private void HandleHorizontalMovement()
+    {
         horizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+    private void HandleJumping()
+    {
+        if (IsGrounded())
+        {
+            coyoteJumpTimer = coyoteJumpTimerMax;
+        }
+        else
+        {
+            coyoteJumpTimer -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferingTimer = jumpBufferingTimerMax;
+        }
+        else
+        {
+            jumpBufferingTimer -= Time.deltaTime;
+        }
+
+        if (jumpBufferingTimer > 0f && coyoteJumpTimer > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
@@ -39,25 +78,15 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            coyoteJumpTimer = 0f; // avoid multi-jumping
+            jumpBufferingTimer = 0f;
         }
-
-        Flip();
-
-        if (IsGrounded())
-        {
-            SetPlayerColor(Color.white);
-        }
-
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-    }
-
-    private void FixedUpdate()
-    {
     }
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapArea(groundCheck.position - (groundCheck.localScale * 0.48f), groundCheck.position + groundCheck.localScale * 0.48f, groundLayer);
+        float playerRadius = 0.5f;
+        return Physics2D.OverlapArea(new Vector2(groundCheck.position.x - playerRadius, groundCheck.position.y), new Vector2(groundCheck.position.x + playerRadius, groundCheck.position.y), groundLayer);
     }
 
     private void Flip()
